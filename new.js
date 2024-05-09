de662a7af79a98643a265be910cb7f479f60fdb9
zoharubab <163677528+zoharubab@users.noreply.github.com>
2024-05-09 23:47:16 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,2 @@
+console.log("zoha rubab");
+//console.log(window); this will will not execute on node js;
---
+++ varableconst.js
@@ -0,0 +1,10 @@
+var name = " zoha ";
+let age =10;
+const salary = 100;
+// let age =61; we can't use it again
+// salary=10; we can't use it again and we can't modify
+//var nmae= "muhammad"; we can use it again and also modidy
+var num1 = 100;
+var num2 = 2.5;
+
+console.log(name + num1 +num2);
